0907eeb5d220af9fd35d3a25726b622241eb7364
sh-seunghee <lsh93633@gmail.com>
2020-10-06 14:39:53 +0900

    commit

--- problems/Top100LikedQuestions/Rotate Image.py
+++ problems/Top100LikedQuestions/Rotate Image.py
@@ -22,17 +22,19 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        matrix.reverse()   # Reverse up to down
+        matrix.reverse()  # Reverse up to down (in-place)
 
         for i in range(len(matrix)):
             for j in range(i):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j] # Swap
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]  # Swap
 
         return matrix
 
-S = Solution()
-matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
 
-print (S.rotate((matrix)))
+S = Solution()
+matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+
+print(S.rotate((matrix)))
+
 
 
