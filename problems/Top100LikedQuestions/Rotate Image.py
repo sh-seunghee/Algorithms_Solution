49898f1dbb50e918dc178475f0e78c14f976ebd6
sh-seunghee <lsh93633@gmail.com>
2020-10-06 14:21:11 +0900

    commit

---
+++ problems/Top100LikedQuestions/Rotate Image.py
@@ -0,0 +1,38 @@
+'''
+Rotate Image (medium)
+
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+DO NOT allocate another 2D matrix and do the rotation.
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+'''
+
+'''
+1. Reverse up to down and swap symmetry
+
+'''
+
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        matrix.reverse()   # Reverse up to down
+
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j] # Swap
+
+        return matrix
+
+S = Solution()
+matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+
+print (S.rotate((matrix)))
+
+
